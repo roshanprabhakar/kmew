abe456f974cde6c2eba6556e2e7fd68d467c9faf
Roshan Prabhakar <roshan@aprabhakar.com>
2022-07-24 19:59:40 -0700

    add run script

---
+++ run.sh
@@ -0,0 +1,1 @@
+qemu-system-aarch64 -M raspi3b -kernel build/kmew_os.img -monitor stdio
--- src/kmew_kernel.c
+++ src/kmew_kernel.c
@@ -6,16 +6,6 @@
  * helpful:
  * https://unix.stackexchange.com/questions/118683/what-is-a-device-tree-and-a-device-tree-blob
  */
-#if 0
-int kmew_main(
-  uint64_t dtb_ptr32,
-  uint64_t x1,
-  uint64_t x2,
-  uint64_t x3) {
-
-}
-#endif
-
-void kmew_main(uint64_t dtb_ptr32) {
+int kmew_main(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3) {
 
 }
